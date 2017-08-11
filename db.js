96a071e7e0f567366afa6a64626df841458ff32e
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-08-11 11:40:41 +0300

    submissions db

--- db.js
+++ db.js
@@ -14,17 +14,32 @@ db.configure({
 
 const TABLE_SUBMISSIONS = '';
 
+const Fields = {
+    courseId: 'course_id',
+    profileId: 'profile_id',
+    cost: 'cost',
+    submissionId: 'id',
+    status: 'status',
+    timestamp: 'update_at'
+};
+
 module.exports = {
-    insertSubmission: (submission) => {
-        return db.query(`INSERT INTO ${TABLE_SUBMISSIONS} VALUES (${submission.course_id}, ${submission.profile_id}, ${submission.cost}, ${submission.id}, ${submission.status}, NOW())`)
+    updateSubmissionStatus: function (submission) {
+        return db.query(`UPDATE ${TABLE_SUBMISSIONS} SET ${Fields.status} = '${submission.status}' WHERE ${Fields.submissionId} = ${submission.id}`);
     },
-    getLastSubmission: (profileId) => {
-        return db.query(`SELECT * FROM ${TABLE_SUBMISSIONS} WHERE profile_id = ${profileId} ORDER BY timestamp DESC LIMIT 1`);
+    insertSubmission: function (submission) {
+        return db.query(`INSERT INTO ${TABLE_SUBMISSIONS} VALUES (${submission.course_id}, ${submission.profile_id}, ${submission.cost}, ${submission.id}, '${submission.status}', NOW())`);
     },
-    calculateStreak: (profileId) => {
-        return this.getLastSubmission(profileId).then((submission) => {
-            if (submission && submission.status == 'correct') {
-                return submission.cost + 1;
+    getNthSubmissionFromEnd: function (courseId, profileId, position) {
+        return db.query(`SELECT * FROM ${TABLE_SUBMISSIONS} WHERE ${Fields.profileId} = ${profileId} AND ${Fields.courseId} = ${courseId} ORDER BY ${Fields.timestamp} DESC LIMIT ${position}, 1`);
+    }
+    getLastSubmission: function (courseId, profileId) {
+        return this.getNthSubmissionFromEnd(courseId, profileId, 1);
+    },
+    calculateStreak: function (courseId, profileId) {
+        return this.getLastSubmission(courseId, profileId).spread((submission) => {
+            if (submission && submission[Fields.status] == 'correct') {
+                return submission[Fields.cost] + 1;
             } else {
                 return 1;
             }
