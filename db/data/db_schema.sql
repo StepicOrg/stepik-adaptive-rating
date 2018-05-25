63cfad51260bbe97eef1b3b3b545dbe1f3cbf51d
Pavel Sviderski <pavel@psviderski.name>
2018-05-25 20:40:50 +0300

    add some evident indexes to rating and cache tables

--- db/data/db_schema.sql
+++ db/data/db_schema.sql
@@ -13,6 +13,11 @@ CREATE TABLE IF NOT EXISTS `cache` (
   `exp` int(11) NOT NULL,
   `delta` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE INDEX `cache_course` ON `cache` (`course`);
+CREATE INDEX `cache_user` ON `cache` (`user`);
+CREATE INDEX `cache_exp` ON `cache` (`exp`);
+CREATE INDEX `cache_delta` ON `cache` (`delta`);
+CREATE INDEX `cache_course_user` ON `cache` (`course`, `user`);
 
 CREATE TABLE IF NOT EXISTS `rating` (
   `course` int(11) NOT NULL,
@@ -21,6 +26,11 @@ CREATE TABLE IF NOT EXISTS `rating` (
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE INDEX `rating_course` ON `rating` (`course`);
+CREATE INDEX `rating_user` ON `rating` (`user`);
+CREATE INDEX `rating_exp` ON `rating` (`exp`);
+CREATE INDEX `rating_updated_at` ON `rating` (`updated_at`);
+CREATE INDEX `rating_course_user` ON `rating` (`course`, `user`);
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL PRIMARY KEY
